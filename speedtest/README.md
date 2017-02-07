a2d7e18c7e022e13a61ab6bb3eacf5b5f574c26c
Camilo Sarmiento <camilo@quanticmind.com>
2017-02-07 12:12:47 -0500

    Working bitbar plugin that uses `speedtest-cli`

---
+++ speedtest/README.md
@@ -0,0 +1,7 @@
+Speedtest bitbar plugin
+=======================
+* Python comes pre-installed on Mac OS X so it is easy to start using.
+* Make sure you have [`pip`](http://softwaretester.info/install-and-upgrade-pip-on-mac-os-x/) installed and on your `$PATH`.
+* Add [`speedtest-cli`](https://github.com/sivel/speedtest-cli) that is a command line interface for testing internet bandwidth using speedtest.net.
+
+Enjoy seeing your upload and download speed on [`bitbar`](https://getbitbar.com/).
--- speedtest/speedtest.30m.py
+++ speedtest/speedtest.30m.py
@@ -1,7 +1,18 @@
 #!/usr/bin/env python
 
+import re
+import subprocess
+
+
 def main():
-  print ":mushroom:"
+    try:
+        output = subprocess.check_output(["/usr/local/bin/speedtest-cli"])
+        download = re.search('Download: (.*)', output).group(1)
+        upload = re.search('Upload: (.*)', output).group(1)
+        result = '%s :arrow_down: - %s :arrow_up:'
+        print result % (download, upload)
+    except subprocess.CalledProcessError as e:
+        print e.output
 
 
 main()
