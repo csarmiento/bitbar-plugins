61604136cb51571c50944b2cb28a6e7c1353e879
Camilo Sarmiento <camilo@quanticmind.com>
2017-02-07 12:33:12 -0500

    More metadata

--- speedtest/speedtest.30m.py
+++ speedtest/speedtest.30m.py
@@ -1,8 +1,11 @@
 #!/usr/bin/env python
-# <bitbar.title>Speedtest</bitbar.title>
+
+# <bitbar.title>Bitbar speedtest plugin</bitbar.title>
 # <bitbar.version>v0.1</bitbar.version>
 # <bitbar.author>Camilo Sarmiento</bitbar.author>
+# <bitbar.author.github>csarmiento</bitbar.author.github>
 # <bitbar.desc>Monitors upload and download speeds using `speedtest-cli`.</bitbar.desc>
+# <bitbar.dependencies>python</bitbar.dependencies>
 
 import re
 import subprocess
@@ -13,7 +16,7 @@ def main():
         output = subprocess.check_output(["/usr/local/bin/speedtest-cli"])
         download = re.search('Download: (.*)', output).group(1)
         upload = re.search('Upload: (.*)', output).group(1)
-        result = '%s :arrow_down: - %s :arrow_up:'
+        result = '%s :arrow_down:|size=10\n%s :arrow_up:|size=10'
         print result % (download, upload)
     except subprocess.CalledProcessError as e:
         print e.output
